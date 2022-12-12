6c224585617f3d47317e98376a170a746d239223
wexokk <kk@wexo.dk>
2022-12-12 21:41:02 +0100

    choose feedback style and some animation

--- src/components/FeedbackType.vue
+++ src/components/FeedbackType.vue
@@ -1,15 +1,33 @@
 <template>
-	<div class="flex max-w-lg justify-around mx-auto mb-4">
-		<button v-for="type in activeButtons" class="feedback-type" :class="type.name" @click="chooseFeedbackType(type)">
-			{{type.name}}
-		</button>
-	</div>
+		<TransitionGroup name="list" tag="div" class="max-w-lg mb-4 mx-auto" appear>
+			<div v-if="activeButtons.length">
+				<button
+					v-if="activeButtons.length"
+					v-for="type in activeButtons"
+					:key="type"
+					class="feedback-type"
+					:class="type.name"
+					@click="chooseFeedbackType(type)"
+				>
+					{{type.name}}
+				</button>
+			</div>
+			<div v-else>
+				<button
+					:class="activeButton.name"
+					class="feedback-type"
+					@click="chooseFeedbackType(activeButton)"
+				>
+					{{activeButton.name}}
+				</button>
+			</div>
+		</TransitionGroup>
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 
-const buttonsArray = [
+const buttonsArray = ref([
 	{
 		name: 'ris',
 		active: false
@@ -22,20 +40,34 @@ const buttonsArray = [
 		name: 'ros',
 		active: false
 	}
-]
+])
 
 const activeButtons = computed(() => {
-	return buttonsArray.find((button) => {
+	return buttonsArray.value.some((button) => {
 		return button.active
-	}) ?? buttonsArray
+	}) ? [] : buttonsArray
+})
+
+const activeButton = computed(() => {
+	return buttonsArray.value.find((button) => {
+		return button.active
+	})
 })
 
 const chooseFeedbackType = (type: any) => {
-	console.log(type)
+	type.active = !type.active;
 }
 </script>
 
 <style lang="scss">
+.list-enter-active,
+.list-leave-active {
+	transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+	opacity: 0;
+}
 .feedback-type {
 	@apply min-w-[9rem] border rounded-md focus:outline-none;
 	&.ros {
